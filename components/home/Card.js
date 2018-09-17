bceac81d92f619fa4be7fc72712e00a4a3e8de6f
toasttify <ipod22433@gmail.com>
2018-09-17 14:51:29 -0500

    Formatting

--- components/home/Card.js
+++ components/home/Card.js
@@ -1,55 +1,53 @@
-import React, { Component } from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import { CardViewWithImage } from 'react-native-simple-card-view'
-import { AppStore } from '../AppStore/AppStore'
-
+import React, { Component } from "react"
+import { View, Text, Dimensions } from "react-native"
+import { CardViewWithImage } from "react-native-simple-card-view"
+import { AppStore } from "../AppStore/AppStore"
 
 export default class Card extends Component {
   constructor(props) {
     super(props)
-    let width = Dimensions.get('window').width - 20
-    let height = Dimensions.get('window').height / 8
-    
+    let width = Dimensions.get("window").width - 20
+    let height = Dimensions.get("window").height / 8
+
     this.state = {
       width: width,
-      height: height,
-    };
-  };
+      height: height
+    }
+  }
 
-  componentWillMount(){
+  componentWillMount() {
     this.fetchAssetGroupID()
   }
 
-  fetchGroupData = (group_id) => {
-    let xhr = new XMLHttpRequest();
+  fetchGroupData = group_id => {
+    let xhr = new XMLHttpRequest()
 
     refreshComp = () => {
       this.forceUpdate()
     }
 
-    xhr.open("GET", `https://login.assetpanda.com/v2/entities/${AppStore.asset_id}/objects`, true);
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.setRequestHeader('Authorization', `Bearer ${AppStore.client_token}`)
-    xhr.send(JSON.stringify({
-    }))
+    xhr.open(
+      "GET",
+      `https://login.assetpanda.com/v2/entities/${AppStore.asset_id}/objects`,
+      true
+    )
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.setRequestHeader("Authorization", `Bearer ${AppStore.client_token}`)
+    xhr.send(JSON.stringify({}))
 
-    xhr.onload = function () {
+    xhr.onload = function() {
       if (xhr.status === 200) {
-        var data = JSON.parse(this.responseText);
+        var data = JSON.parse(this.responseText)
         AppStore.image_id = data.objects[0].default_attachment.medium
         AppStore.primary_default = data.objects[0].display_name
         AppStore.secondary_default = data.objects[0].field_5
         alert(`The image url is ${AppStore.image_id}`)
         refreshComp()
-
-      }
-      else if (xhr.status === 502) {
+      } else if (xhr.status === 502) {
         alert("502 bad gateway error, please try again in a few minutes")
-      }
-      else if (xhr.status === 500) {
+      } else if (xhr.status === 500) {
         alert("Internal server error")
-      }
-      else{
+      } else {
         alert(`Some other error occured code ${xhr.status}`)
       }
     }
@@ -57,54 +55,49 @@ export default class Card extends Component {
 
   fetchAssetGroupID = () => {
     //Sends credentials to api and stores token, also navigates to Home screen upon success
-    let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest()
 
     refreshComp = () => {
       this.forceUpdate()
     }
 
-    getGroupInfo = (group_id) => {
+    getGroupInfo = group_id => {
       this.fetchGroupData(group_id)
     }
 
-    xhr.open("GET", 'https://login.assetpanda.com/v2/entities', true);
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.setRequestHeader('Authorization', `Bearer ${AppStore.client_token}`)
-    xhr.send(JSON.stringify({
-    }))
+    xhr.open("GET", "https://login.assetpanda.com/v2/entities", true)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.setRequestHeader("Authorization", `Bearer ${AppStore.client_token}`)
+    xhr.send(JSON.stringify({}))
 
-    xhr.onload = function () {
+    xhr.onload = function() {
       if (xhr.status === 200) {
-        var data = JSON.parse(this.responseText);
+        var data = JSON.parse(this.responseText)
         AppStore.asset_id = data[0].id
         alert(`The group id is ${AppStore.asset_id}`)
         refreshComp()
         getGroupInfo()
-
-      }
-      else if (xhr.status === 502) {
+      } else if (xhr.status === 502) {
         alert("502 bad gateway error, please try again in a few minutes")
-      }
-      else if (xhr.status === 500) {
+      } else if (xhr.status === 500) {
         alert("Internal server error")
       }
     }
   }
 
-
   render() {
     return (
       <View>
-            <CardViewWithImage
-                width={this.state.width}
-                source={{uri: AppStore.image_id}}
-                content={AppStore.secondary_default}
-                title={AppStore.primary_default}
-                roundedImage={false}
-                imageWidth={300}
-                imageHeight={this.state.height}
-            />
+        <CardViewWithImage
+          width={this.state.width}
+          source={{ uri: AppStore.image_id }}
+          content={AppStore.secondary_default}
+          title={AppStore.primary_default}
+          roundedImage={false}
+          imageWidth={300}
+          imageHeight={this.state.height}
+        />
       </View>
-    );
+    )
   }
 }
--- components/home/Home.js
+++ components/home/Home.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
-import { ScrollView, Text, Container, Dimensions, Keyboard } from 'react-native';
-import Card from './Card'
-import { CardViewWithImage } from 'react-native-simple-card-view'
-import { AppStore } from '../AppStore/AppStore'
+import React, { Component } from "react"
+import { ScrollView, Text, Container, Dimensions, Keyboard } from "react-native"
+import Card from "./Card"
+import { CardViewWithImage } from "react-native-simple-card-view"
+import { AppStore } from "../AppStore/AppStore"
 
 export default class Home extends Component {
   constructor(props) {
-    let screen_width = (Dimensions.get('window').width) - 20
-    super(props);
+    let screen_width = Dimensions.get("window").width - 20
+    super(props)
     this.state = {
       card_width: screen_width
-    };
-
+    }
   }
 
   static navigationOptions = {
-    title: 'Assets' ,
-    headerMode: 'screen',
-    color: 'blue'
-
-  };
+    title: "Assets",
+    headerMode: "screen",
+    color: "blue"
+  }
 
   render() {
     return (
       <ScrollView>
         <Card />
       </ScrollView>
-    );
+    )
   }
 }
